39cc61722476bee378b561ac7ec0c21ba35c41a4
egaxiola83 <egaxiola83@gmail.com>
2020-11-30 11:57:31 -0600

    2 test

---
+++ Addition.java
@@ -0,0 +1,15 @@
+public class Addition {
+  private double Number1, Number2;
+  
+  public void setNumber1(double Number1) {
+    this.Number1 = Number1;
+  }
+  
+  public void setNumber2(double Number2) {
+    this.Number2 = Number2;
+  }
+  
+  public double Result() {
+    return Number1+Number2;
+  }
+} 
